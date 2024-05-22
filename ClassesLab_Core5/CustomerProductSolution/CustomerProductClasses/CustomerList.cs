a78ec9699586b622a8216b10399a9ddaa8ac063c
travisburns <travisjb0@gmail.com>
2024-05-21 17:36:49 -0700

    Finished Lab 5 question 1

---
+++ ClassesLab_Core5/CustomerProductSolution/CustomerProductClasses/CustomerList.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace CustomerProductClasses
+{
+    public class CustomerList
+    {
+        private List<Customer> customers;
+
+        public CustomerList()
+        {
+            customers = new List<Customer>();
+        }
+
+        public int Count
+        {
+            get { return customers.Count; }
+        }
+
+        public Customer this[int index]
+        {
+            get { return customers[index]; }
+            set { customers[index] = value; }
+        }
+
+        public Customer GetCustomerByEmail(string email)
+        {
+            return customers.FirstOrDefault(c => c.Email == email);
+        }
+
+        public void Add(Customer customer)
+        {
+            customers.Add(customer);
+        }
+
+        public void Add(int id, string firstName, string lastName, string email, string phoneNumber)
+        {
+            Customer customer = new Customer(id, firstName, lastName, email, phoneNumber);
+            customers.Add(customer);
+        }
+
+        public static CustomerList operator +(CustomerList list, Customer customer)
+        {
+            list.Add(customer);
+            return list;
+        }
+
+        public void Save()
+        {
+            CustomerDB.SaveCustomers(customers);
+        }
+
+        public void Fill()
+        {
+            customers = CustomerDB.GetCustomers();
+        }
+
+        public void Remove(Customer customer)
+        {
+            customers.Remove(customer);
+        }
+
+        public static CustomerList operator -(CustomerList list, Customer customer)
+        {
+            list.Remove(customer);
+            return list;
+        }
+
+        public override string ToString()
+        {
+            string output = "";
+            foreach (Customer c in customers)
+            {
+                output += c.ToString() + "\n";
+            }
+            return output;
+        }
+    }
+}
--- ClassesLab_Core5/CustomerProductSolution/CustomerProductTests/Program.cs
+++ ClassesLab_Core5/CustomerProductSolution/CustomerProductTests/Program.cs
@@ -1,5 +1,6 @@
 ﻿//this was previously the test for the TestProduct, used this for the CustomerProductTests and the orignal code is below in comments. 
 using System;
+using System.Collections.Generic;
 using CustomerProductClasses;
 
 namespace CustomerProductTests
@@ -8,127 +9,282 @@ namespace CustomerProductTests
     {
         static void Main(string[] args)
         {
-            TestCustomerConstructors();
-            TestCustomerToString();
-            TestCustomerPropertyGetters();
-            TestCustomerPropertySetters();
+            Console.SetWindowSize(120, 40);
+            //TestCustomerConstructors();
+            // TestCustomerToString();
+            //TestCustomerPropertyGetters();
+            //TestCustomerPropertySetters();
+            // TestProductListConstructor();
+            //TestProductListAdd();
+            //TestProductListSaveAndFill();
+            //TestProductListRemove();
+            //TestProductEquals();
+            //TestProductGetHashCode();
+            //TestProductListIndexer();
 
-            Console.WriteLine("Tests complete");
-            Console.ReadKey();
+            //Testing CustomerList from this point on
+            TestCustomerListConstructor();
+            TestCustomerListAdd();
+            TestCustomerListSaveAndFill();
+            TestCustomerListRemove();
+            TestCustomerEquals();
+            TestCustomerGetHashCode();
+            TestCustomerListIndexer();
+            TestCustomerListGetCustomerByEmail();
+
+            Console.ReadLine();
         }
 
-        static void TestCustomerConstructors()
-        {
-            Customer c1 = new Customer();
-            Customer c2 = new Customer(1, "Travis", "Burns", "travisjb0@gmail.com", "987-432-9531");
 
-            Console.WriteLine("Testing both constructors");
-            Console.WriteLine("Default constructor.  Expecting default values. " + c1.ToString());
-            Console.WriteLine("Overloaded constructor.  Expecting 1, Travis, Burns, travisjb0@gmail.com, 987-432-9531 " + c2.ToString());
+
+
+
+
+
+
+        static void TestProductListConstructor()
+        {
+            ProductList list = new ProductList();
+
+            Console.WriteLine("Testing product list default constructor");
+            Console.WriteLine("Count.  Expecting 0. " + list.Count);
+            Console.WriteLine("ToString.  Expect an empty string. " + list.ToString());
             Console.WriteLine();
         }
 
-        static void TestCustomerToString()
+        static void TestProductListAdd()
         {
-            Customer c1 = new Customer(1, "Travis", "Burns", "travisjb0@gmail.com", "987-432-9531");
+            ProductList list = new ProductList();
+            Product p1 = new Product(1, "T100", "This is a test product", 100M, 10);
+            Product p2 = new Product(2, "T200", "This is a test product 2", 200M, 20);
 
-            Console.WriteLine("Testing ToString");
-            Console.WriteLine("Expecting Id: 1, Name: Travis Burns, Email: travisjb0@gmail.com, Phone: 987-432-9531 " + c1.ToString());
+            Console.WriteLine("Testing product list add.");
+            Console.WriteLine("BEFORE Count.  Expecting 0. " + list.Count);
+            list.Add(p1);
+            Console.WriteLine("AFTER Add Count.  Expecting 4. " + list.Count);
+            Console.WriteLine("ToString.  Expect four products " + list.ToString());
+
+            list = list + p2;
+            list = p2 + list;
+            list += p2;
+            Console.WriteLine("AFTER Second Add Count.  Expecting 2. " + list.Count);
+            Console.WriteLine("ToString.  Expect two products " + list.ToString());
             Console.WriteLine();
         }
 
-        static void TestCustomerPropertyGetters()
+        static void TestProductListSaveAndFill()
         {
-            Customer c1 = new Customer(1, "Travis", "Burns", "travisjb0@gmail.com", "987-432-9531");
+            ProductList list = new ProductList();
+            Product p1 = new Product(1, "T100", "This is a test product", 100M, 10);
+            Product p2 = new Product(2, "T200", "This is a test product 2", 200M, 20);
+            list.Add(p1);
+            list += p2;
+            list.Save();
 
-            Console.WriteLine("Testing getters");
-            Console.WriteLine("Id.  Expecting 1. " + c1.Id);
-            Console.WriteLine("FirstName.  Expecting Travis. " + c1.FirstName);
-            Console.WriteLine("LastName.  Expecting Burns. " + c1.LastName);
-            Console.WriteLine("Email.  Expecting travisjb0@gmail.com " + c1.Email);
-            Console.WriteLine("PhoneNumber.  Expecting 987-432-9531. " + c1.PhoneNumber);
+            list = new ProductList();
+            list.Fill();
+            Console.WriteLine("Testing product list save and fill.");
+            Console.WriteLine("After Fill Count.  Expecting 2. " + list.Count);
+            Console.WriteLine("ToString.  Expect two products " + list.ToString());
             Console.WriteLine();
         }
 
-        static void TestCustomerPropertySetters()
+
+        static void TestProductEquals()
         {
-            Customer c1 = new Customer(1, "Travis", "Burns", "travisjb0@gmail.com", "987-432-9531");
-
-            Console.WriteLine("Testing setters");
-            c1.FirstName = "Tom";
-            Console.WriteLine("Expecting Tom. " + c1.FirstName);
-            Console.WriteLine();
-        }
-    }
-}
-
-
-
-
-/* using System;
-using CustomerProductClasses;
-
-namespace CustomerProductTests
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-
-            TestProductConstructors();
-            TestProductToString();
-            TestProductPropertyGetters();
-            TestProductPropertySetters();
-        }
-
-        static void TestProductConstructors()
-        {
-            Product p1 = new Product();
+            // these 2 objects should be equal.  They reference the same object.
+            Product p1 = new Product(1, "T100", "This is a test product", 100M, 10);
+            Product p1Reference = p1;
+            // these 2 objects should be equal after overridding Equals.  The attribute values are all equal.
             Product p2 = new Product(1, "T100", "This is a test product", 100M, 10);
 
-            Console.WriteLine("Testing both constructors");
-            Console.WriteLine("Default constructor.  Expecting default values. " + p1.ToString());
-            Console.WriteLine("Overloaded constructor.  Expecting 1, T100, 100, This is a test product, 10 " + p2.ToString());
+            Console.WriteLine("Testing product equals.");
+            Console.WriteLine("2 references to the same object.  Expecting true. " + p1.Equals(p1Reference));
+            Console.WriteLine("2 object that have the same properties should be equal.  Expecting true. " + p1.Equals(p2));
+
+
+            Console.WriteLine("Testing product ==");
+            Console.WriteLine("2 references to the same object.  Expecting true. " + (p1 == p1Reference));
+            Console.WriteLine("2 object that have the same properties should be equal.  Expecting true. " + (p1 == p2));
+
+            Console.WriteLine();
+
+        }
+
+
+        static void TestProductListRemove()
+        {
+            // test fails before I add equals to product
+            ProductList list = new ProductList();
+            Product p1 = new Product(1, "T100", "This is a test product", 100M, 10);
+
+            list.Fill();
+            Console.WriteLine("Testing product list remove.");
+            Console.WriteLine("Before remove Count.  Expecting 2. " + list.Count);
+            Console.WriteLine("ToString.  Expect two products " + list.ToString());
+            //list.Remove(p1);
+            //list -= p1;
+            list -= 2;
+            Console.WriteLine("After remove Count.  Expecting 1. " + list.Count);
+            Console.WriteLine("ToString.  Expect empty list " + list.ToString());
             Console.WriteLine();
         }
 
-        static void TestProductToString()
+
+        static void TestProductGetHashCode()
         {
             Product p1 = new Product(1, "T100", "This is a test product", 100M, 10);
+            // these 2 objects should have the same hashcode.  The attribute values are all equal.
+            Product p2 = new Product(1, "T100", "This is a test product", 100M, 10);
+            // this one should have a unique hashcode
+            Product p3 = new Product(3, "T300", "This is a test product 3", 300M, 30);
 
-            Console.WriteLine("Testing ToString");
-            Console.WriteLine("Expecting 1, T100, 100, This is a test product, 10 " + p1.ToString());
-            Console.WriteLine("Expecting 1, T100, 100, This is a test product, 10 " + p1);
+            Console.WriteLine("Testing product GetHashCode");
+            Console.WriteLine("2 object that have the same properties should have the same hashcode.  Expecting true. " + (p1.GetHashCode() == p2.GetHashCode()));
+            Console.WriteLine("2 object that have different properties should have different hashcodes.  Expecting false. " + (p1.GetHashCode() == p3.GetHashCode()));
+
+            // this will fail before overriding GetHashCode
+            HashSet<Product> set = new HashSet<Product>();
+            set.Add(p1);
+            set.Add(p3);
+            Console.WriteLine("Testing product GetHashCode by using a hash set");
+            Console.WriteLine("The hash set should be able to find an object with the same attributes.  Expecting true. " + set.Contains(p2));
+
             Console.WriteLine();
         }
 
-        static void TestProductPropertyGetters()
+        static void TestProductListIndexer()
         {
-            Product p1 = new Product(1, "T100", "This is a test product", 100M, 10);
+            // test fails before I add equals to product
+            ProductList list = new ProductList();
+            list.Fill();
 
-            Console.WriteLine("Testing getters");
-            Console.WriteLine("Id.  Expecting 1. " + p1.Id);
-            Console.WriteLine("Code.  Expecting T100. " + p1.Code);
-            Console.WriteLine("Description.  Expecting This is a test product. " + p1.Description);
-            Console.WriteLine("Price.  Expecting 100. " + p1.UnitPrice);
-            Console.WriteLine("Quantity.  Expecting 10. " + p1.QuantityOnHand);
+            Console.WriteLine("Testing product list indexer");
+            Console.WriteLine("Index 0.  Expecting first product in list to be T100 \n" + list[0]);
+            Console.WriteLine("Index 'T200'.  Expecting product with code of T200 \n" + list["T200"]);
             Console.WriteLine();
         }
 
-        static void TestProductPropertySetters()
-        {
-            Product p1 = new Product(1, "T100", "This is a test product", 100M, 10);
 
-            Console.WriteLine("Testing setters");
-            p1.Id = 2;
-            p1.Code = "T000";
-            p1.Description = "First product";
-            p1.UnitPrice = 200;
-            p1.QuantityOnHand = 20;
-            Console.WriteLine("Expecting 2, T000, 200, First product, 20 " + p1);
+
+
+
+
+        //Testing CustomerList at this point 
+
+        static void TestCustomerListConstructor()
+        {
+            CustomerList list = new CustomerList();
+
+            Console.WriteLine("Testing customer list default constructor");
+            Console.WriteLine("Count.  Expecting 0. " + list.Count);
+            Console.WriteLine("ToString.  Expect an empty string. " + list.ToString());
+            Console.WriteLine();
+        }
+
+        static void TestCustomerListAdd()
+        {
+            CustomerList list = new CustomerList();
+            Customer c1 = new Customer(1, "Travis", "Burns", "travisjb0@example.com", "123-456-7890");
+            Customer c2 = new Customer(2, "Sarah", "Yvonette", "Sarahyv0@example.com", "987-654-3210");
+
+            Console.WriteLine("Testing customer list add.");
+            Console.WriteLine("BEFORE Count.  Expecting 0. " + list.Count);
+            list.Add(c1);
+            Console.WriteLine("AFTER Add Count.  Expecting 1. " + list.Count);
+            Console.WriteLine("ToString.  Expect one customer " + list.ToString());
+
+            list = list + c2;
+            list += c2;
+            list += c2;
+            Console.WriteLine("AFTER Second Add Count.  Expecting 2. " + list.Count);
+            Console.WriteLine("ToString.  Expect two customers " + list.ToString());
+            Console.WriteLine();
+        }
+
+        static void TestCustomerListSaveAndFill()
+        {
+            CustomerList list = new CustomerList();
+            Customer c1 = new Customer(1, "Travis", "Burns", "travisjb0@example.com", "123-456-7890");
+            Customer c2 = new Customer(2, "Dan", "Thomas", "Dt@gmail.com", "987-654-3210");
+            list.Add(c1);
+            list += c2;
+            list.Save();
+
+            list = new CustomerList();
+            list.Fill();
+            Console.WriteLine("Testing customer list save and fill.");
+            Console.WriteLine("After Fill Count.  Expecting 2. " + list.Count);
+            Console.WriteLine("ToString.  Expect two customers " + list.ToString());
+            Console.WriteLine();
+        }
+
+        static void TestCustomerListRemove()
+        {
+            CustomerList list = new CustomerList();
+            Customer c1 = new Customer(1, "Travis", "Burns", "travisjb@gmail.com", "123-456-7890");
+
+            list.Add(c1);
+            Console.WriteLine("Testing customer list remove.");
+            Console.WriteLine("Before remove Count.  Expecting 1. " + list.Count);
+            Console.WriteLine("ToString.  Expect one customer " + list.ToString());
+            list.Remove(c1);
+            list -= c1;
+            Console.WriteLine("After remove Count.  Expecting 0. " + list.Count);
+            Console.WriteLine("ToString.  Expect empty list " + list.ToString());
+            Console.WriteLine();
+        }
+
+        static void TestCustomerEquals()
+        {
+            Customer c1 = new Customer(1, "Travis", "Burns", "travis@gmail.com", "123-456-7890");
+            Customer c2 = new Customer(1, "Sarah", "Burns", "sarah@gmail.com", "123-456-7890");
+
+            Console.WriteLine("Testing customer equals.");
+            Console.WriteLine("Expecting true: " + c1.Equals(c2));
+            Console.WriteLine();
+        }
+
+        static void TestCustomerGetHashCode()
+        {
+            Customer c1 = new Customer(1, "Travis", "Burns", "Travis@gmail.com", "123-456-7890");
+            Customer c2 = new Customer(1, "Sarah", "Burns", "Sarah@example.com", "123-456-7890");
+
+            Console.WriteLine("Testing customer GetHashCode.");
+            Console.WriteLine("Expecting equal hash codes: " + (c1.GetHashCode() == c2.GetHashCode()));
+            Console.WriteLine();
+        }
+
+        static void TestCustomerListIndexer()
+        {
+            CustomerList list = new CustomerList();
+            Customer c1 = new Customer(1, "Travis", "Burns", "Travis@gmail.com", "123-456-7890");
+            Customer c2 = new Customer(2, "Sarah", "Burns", "Sarah@gmail.com", "987-654-3210");
+            list.Add(c1);
+            list.Add(c2);
+
+            Console.WriteLine("Testing customer list indexer");
+            Console.WriteLine("Index 0.  Expecting first customer in list to be Travis Burns \n" + list[0]);
+            Console.WriteLine("Index 1.  Expecting second customer in list to be Sarah Burns \n" + list[1]);
+            Console.WriteLine();
+        }
+
+        static void TestCustomerListGetCustomerByEmail()
+        {
+            CustomerList list = new CustomerList();
+            Customer c1 = new Customer(1, "Travis", "Burns", "Travisjb0@gmail.com", "123-456-7890");
+            Customer c2 = new Customer(2, "Sarah", "Burns", "Sarah@gmail.com", "987-654-3210");
+            list.Add(c1);
+            list.Add(c2);
+
+            Console.WriteLine("Testing customer list GetCustomerByEmail");
+            Console.WriteLine("Email 'travisjb0@gmail.com'.  Expecting Travis Burns \n" + list.GetCustomerByEmail("Travisjb0@gmail.com"));
+            Console.WriteLine("Email 'sarah@gmail.com'.  Expecting Sarah Burns \n" + list.GetCustomerByEmail("Sarah@gmail.com"));
             Console.WriteLine();
         }
     }
 }
 
-*/
+
+
+
